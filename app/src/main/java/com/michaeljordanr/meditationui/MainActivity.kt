fd40d5dfe56fa2a8209f473359006e0f29029d28
Michael Jordan <michaeljordanribeiro@gmail.com>
2023-06-17 16:44:15 -0300

    testing some components with Jetpack Compose

---
+++ app/src/main/java/com/michaeljordanr/meditationui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.michaeljordanr.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.michaeljordanr.meditationui.ui.HomeScreen
+import com.michaeljordanr.meditationui.ui.theme.MeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUITheme {
+                HomeScreen()
+            }
+        }
+    }
+}
